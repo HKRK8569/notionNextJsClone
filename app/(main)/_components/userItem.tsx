89afe41ee8972134163b4baa12fa28ea42cbcfbb
HKRK8569 <kayann3021@gmail.com>
2024-03-10 22:47:47 +0900

    dropdownMenuの修正

---
+++ app/(main)/_components/userItem.tsx
@@ -0,0 +1,35 @@
+"use client";
+import { Avatar, AvatarImage } from "@radix-ui/react-avatar";
+import { useUser } from "@clerk/clerk-react";
+
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from "@radix-ui/react-dropdown-menu";
+
+export const UserItem = () => {
+  const { user } = useUser();
+  return (
+    <DropdownMenu>
+      <DropdownMenuTrigger asChild>
+        <div
+          role="button"
+          className="flex items-center text-sm p-3 w-full hover:bg-primary/5"
+        >
+          <div className="gap-x-2 flex items-center max-w-[150px] ">
+            <Avatar className="h-5 w-5">
+              <AvatarImage src={user?.imageUrl} />
+            </Avatar>
+            <span className="text-start font-medium line-clamp-1">
+              {user?.username}&apos;s Notion
+            </span>
+          </div>
+        </div>
+      </DropdownMenuTrigger>
+    </DropdownMenu>
+  );
+};
