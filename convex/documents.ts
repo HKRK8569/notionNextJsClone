88d497017de730ffeac98743a3462958eced3382
HKRK8569 <kayann3021@gmail.com>
2024-04-07 05:08:53 +0900

    documet追加関数の作成

--- convex/documents.ts
+++ convex/documents.ts
@@ -1,30 +1,44 @@
-import {v} from "convex/values"
-import { mutation, query} from "./_generated/server"
-import {Doc, Id} from "./_generated/dataModel"
+import { v } from "convex/values";
+import { mutation, query } from "./_generated/server";
+import { Doc, Id } from "./_generated/dataModel";
 
+// documentsテーブルのschemaを取得する関数
+export const get = query({
+  handler: async (ctx) => {
+    const identity = await ctx.auth.getUserIdentity();
+
+    if (!identity) {
+      throw new Error("Not authenticated");
+    }
+
+    const documents = await ctx.db.query("documents").collect()
+
+    return documents;
+  },
+});
 // schemaを作成する関数
 export const create = mutation({
-    args: {
-        title: v.string(),
-        parentDocument: v.optional(v.id("documents"))
-    },
-    handler: async (ctx, args) => {
-        const identity = await ctx.auth.getUserIdentity()
+  args: {
+    title: v.string(),
+    parentDocument: v.optional(v.id("documents")),
+  },
+  handler: async (ctx, args) => {
+    const identity = await ctx.auth.getUserIdentity();
 
-        if (!identity){
-            throw new Error("Not authenticated")
-        }
-
-        const userId = identity.subject;
-
-        const document = await ctx.db.insert("documents",{
-            title: args.title,
-            parentDocument: args.parentDocument,
-            userId,
-            isArchived: false,
-            isPublished: false,
-        })
-
-        return document
+    if (!identity) {
+      throw new Error("Not authenticated");
     }
-})
+
+    const userId = identity.subject;
+
+    const document = await ctx.db.insert("documents", {
+      title: args.title,
+      parentDocument: args.parentDocument,
+      userId,
+      isArchived: false,
+      isPublished: false,
+    });
+
+    return document;
+  },
+});
