269d05688b6b7009186930a0fdf9256665b83e5b
HKRK8569 <kayann3021@gmail.com>
2025-03-29 03:36:16 +0900

    検索機能の作成

--- app/(main)/_components/navigation.tsx
+++ app/(main)/_components/navigation.tsx
@@ -25,8 +25,10 @@ import {
   PopoverTrigger,
 } from "@/components/ui/popover";
 import { TrashBox } from "./trash-box";
+import { useSearch } from "@/hooks/use-search";
 
 export const Navigation = () => {
+  const search = useSearch();
   const pathname = usePathname();
   const isMobile = useMediaQuery("(max-width: 768px)");
 
@@ -148,7 +150,7 @@ export const Navigation = () => {
         </div>
         <div>
           <UserItem />
-          <Item label="Search" Icon={Search} isSearch onClick={() => {}} />
+          <Item label="Search" Icon={Search} isSearch onClick={search.onOpen} />
           <Item label="Settings" Icon={Settings} onClick={() => {}} />
           <Item onClick={handleCreate} label="New Page" Icon={PlusCircle} />
         </div>
