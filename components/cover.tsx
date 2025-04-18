9cacc96cfa97e9dddb1fed046c29126c091f9a61
HKRK8569 <kayann3021@gmail.com>
2025-04-19 00:56:08 +0900

    ドキュメントページにスケルトンを追加

--- components/cover.tsx
+++ components/cover.tsx
@@ -10,6 +10,7 @@ import { api } from "@/convex/_generated/api";
 import { useParams } from "next/navigation";
 import { Id } from "@/convex/_generated/dataModel";
 import { useEdgeStore } from "@/lib/edgestore";
+import { Skeleton } from "./ui/skeleton";
 
 interface CoverImageProps {
   url?: string;
@@ -67,4 +68,8 @@ export const Cover = ({ url, preview }: CoverImageProps) => {
       )}
     </div>
   );
+};
+
+Cover.Skelton = function CoverSkelton() {
+  return <Skeleton className="w-full h-[12-vh]" />;
 };
